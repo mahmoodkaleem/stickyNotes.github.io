32d64ee3f894df761b8762c7a97518a0d0551588
mahmoodkaleem <doc.mmkaleem@gmail.com>
2023-03-07 13:27:04 +0530

    add color button

--- app.js
+++ app.js
@@ -3,67 +3,113 @@ const main = document.getElementById("main");
 //add note button
 const addButton = document.getElementById("add-btn");
 addButton.addEventListener("click", () => {
-    addNote();
+  addNote();
 });
 
 //add new notes box
 const addNote = (localData = "") => {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = `<h1 id="tool-bar">
-<i class="save fa-solid fa-floppy-disk"></i>
-<i class="trash fa-solid fa-trash"></i>
-</h1>
-<textarea name="" id="textarea" >${localData}</textarea>`;
-    main.appendChild(box);
+  const box = document.createElement("div");
+  box.classList.add("box");
+  box.innerHTML = `<h1 id="tool-bar">
+  <i class="font-color fa-solid fa-pen"></i>
+  <i class="color fa-sharp fa-solid fa-paintbrush"></i>
+  <i class="save fa-solid fa-floppy-disk"></i>
+  <i class="trash fa-solid fa-trash"></i>
+  </h1>
+  <textarea name="Title" id="textarea" >${localData}</textarea>`;
+  main.appendChild(box);
 
-    //delete notes box
-    const trashBtn = box.querySelector(".trash");
-    trashBtn.addEventListener("click", () => {
-        deleteNotes();
+  //delete notes box
+  const trashBtn = box.querySelector(".trash");
+  trashBtn.addEventListener("click", () => {
+    deleteNotes();
+  });
+
+  //delete function
+  const deleteNotes = () => {
+    box.remove();
+    saveNotes();
+  };
+
+  //save notes by onClick
+  box.querySelector(".save").addEventListener("click", () => {
+    saveNotes();
+  });
+
+  //save notes by focusOut
+  box.querySelector("textarea").addEventListener("focusout", () => {
+    saveNotes();
+  });
+
+  //saveNotes function with localstorage
+  const saveNotes = () => {
+    const notes = document.querySelectorAll("textarea");
+    let data = [];
+    notes.forEach((note) => {
+      data.push(note.value);
     });
+    if (data.length == 0) {
+      localStorage.removeItem("notes");
+    } else {
+      localStorage.setItem("notes", data);
+    }
+    
+  };
 
-    //delete function
-    const deleteNotes = () => {
-        box.remove();
-        saveNotes();
-    };
+  //background color buttton
+  const colorBtn = box.querySelector(".color");
+  colorBtn.addEventListener("click", () => {
+    bgColorCode();
+  });
 
-    //save notes by onClick
-    box.querySelector(".save").addEventListener("click", () => {
-        saveNotes();
-    });
+  //function for randomcolor of textarea background
+  const bgColorCode = ( ) => {
+    let textarea = box.querySelector("textarea");
+    ColorNum = Math.floor(Math.random() * 16777215);
+    hexCode = "#" + ColorNum.toString(16);
+    if (hexCode.length < 7) {
+      hexCode = hexCode + 1;
+      textarea.style.backgroundColor = hexCode;
+      colorBtn.style.color= hexCode;
+    } else {
+      textarea.style.backgroundColor = hexCode;
+      colorBtn.style.color= hexCode;
+    }
+  };
 
-    //save notes by focusOut
-    box.querySelector("textarea").addEventListener("focusout", () => {
-        saveNotes();
-    });
+//font color buttton
+const fontColorBtn = box.querySelector(".font-color");
+fontColorBtn.addEventListener("click", () => {
+  fontColorCode();
+});
 
-    //saveNotes function with localstorage
-    const saveNotes = () => {
-        const notes = document.querySelectorAll("textarea");
-        let data = [];
-        notes.forEach((note) => {
-            data.push(note.value);
-        });
-        if (data.length == 0) {
-            localStorage.removeItem("notes");
-        } else {
-            localStorage.setItem("notes", data);
-        }
-    };
+//function for randomcolor of textarea font
+const fontColorCode = () => {
+    let textarea = box.querySelector("textarea");
+    ColorNum = Math.floor(Math.random() * 16777215);
+    hexCode = "#" + ColorNum.toString(16);
+    localStorage.setItem("color",hexCode)
+    if (hexCode.length < 7) {
+      hexCode = hexCode + 1;
+      textarea.style.color = hexCode;
+      fontColorBtn.style.color= hexCode;
+    } else {
+      textarea.style.color = hexCode;
+      fontColorBtn.style.color= hexCode;
+    }
+  };
 
 };
 
 //iife function for page refrash and resotre saved data
 (() => {
-    let savedNotes = localStorage.getItem("notes");
-    if (savedNotes === null) {
-        addNote()
-    } else {
-        savedNotes = savedNotes.split(",")
-        savedNotes.forEach((savedValue) => {
-            addNote(savedValue)
-        })
-    }
-})()
+  let savedNotes = localStorage.getItem("notes");
+  if (savedNotes === null) {
+    addNote();
+  } else {
+    savedNotes = savedNotes.split(",");
+    savedNotes.forEach((savedValue) => {
+      addNote(savedValue);
+    });
+  }
+})();
